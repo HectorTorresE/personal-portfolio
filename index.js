9e11533a9f0039610d3cd277335634c641c12449
Hectorjte <hectorjte@gmail.com>
2022-01-24 16:45:48 -0600

    Linter error

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const Menu = document.getElementById('MenuP');
 const headerImg = document.getElementById('headerImg');
 const menuClose = document.getElementById('menuClose');
 
-openAndClose = function () {
-  
+const openAndClose = function () {
   if (Menu.classList.contains('MenuInvisible')){
     Menu.classList.remove('MenuInvisible');
     Menu.classList.add('MenuVisible');
