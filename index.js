6bb741d96f0a588457aadd2ef3381f01fed11483
Hectorjte <hectorjte@gmail.com>
2022-01-24 17:06:30 -0600

    Fix linter error

--- index.js
+++ index.js
@@ -5,23 +5,22 @@ const menuClose = document.getElementById('menuClose');
 const links = document.querySelectorAll('.mobileMenu li a');
 
 const openAndClose = function () {
-  if (Menu.classList.contains("MenuInvisible")){
-    Menu.classList.remove("MenuInvisible");
-    Menu.classList.add("MenuVisible");
-    headerImg.classList.add("MenuInvisible");
-  }
-  else if (Menu.classList.contains("MenuVisible")) {
-    Menu.classList.add("MenuInvisible");
-    Menu.classList.remove("MenuVisible");
-    headerImg.classList.remove("MenuInvisible");
+  if (Menu.classList.contains('MenuInvisible')) {
+    Menu.classList.remove('MenuInvisible');
+    Menu.classList.add('MenuVisible');
+    headerImg.classList.add('MenuInvisible');
+  } else if (Menu.classList.contains('MenuVisible')) {
+    Menu.classList.add('MenuInvisible');
+    Menu.classList.remove('MenuVisible');
+    headerImg.classList.remove('MenuInvisible');
   }
 };
 
 const closeMenuMobile = function () {
-  if (Menu.classList.contains("MenuVisible")) {
-    Menu.classList.add("MenuInvisible");
-    Menu.classList.remove("MenuVisible");
-    headerImg.classList.remove("MenuInvisible");
+  if (Menu.classList.contains('MenuVisible')) {
+    Menu.classList.add('MenuInvisible');
+    Menu.classList.remove('MenuVisible');
+    headerImg.classList.remove('MenuInvisible');
   }
 };
 
@@ -30,4 +29,4 @@ menuClose.addEventListener('click', openAndClose);
 
 links.forEach((element) => {
   element.addEventListener('click', closeMenuMobile);
-})
+});
