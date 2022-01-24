229c14d7f0df334bdea30e4350312e99c3046495
Fernando Silva <77974883+fernando-silvabr66@users.noreply.github.com>
2022-01-24 20:01:26 -0300

    Add index,js file

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-const btnMenu = document.getElementById('btn');
-const Menu = document.getElementById('MenuP');
-const headerImg = document.getElementById('headerImg');
-const menuClose = document.getElementById('menuClose');
+let btnMenu = document.getElementById('btn');
+let Menu = document.getElementById('MenuP');
+let headerImg = document.getElementById('headerImg');
+let menuClose = document.getElementById('menuClose');
+let links = document.querySelectorAll('.mobileMenu li a');
+
+
 
 const openAndClose = function () {
-  if (Menu.classList.contains('MenuInvisible')){
-    Menu.classList.remove('MenuInvisible');
-    Menu.classList.add('MenuVisible');
-    headerImg.classList.add('MenuInvisible');
-  } else if (Menu.classList.contains('MenuVisible')) {
-    Menu.classList.add('MenuInvisible');
-    Menu.classList.remove('MenuVisible');
-    headerImg.classList.remove('MenuInvisible');
+  if (Menu.classList.contains("MenuInvisible")){
+    Menu.classList.remove("MenuInvisible");
+    Menu.classList.add("MenuVisible");
+    headerImg.classList.add("MenuInvisible");
+  }
+  else if (Menu.classList.contains("MenuVisible")) {
+    Menu.classList.add("MenuInvisible");
+    Menu.classList.remove("MenuVisible");
+    headerImg.classList.remove("MenuInvisible");
+  }
+};
+
+const closeMenuMobile = function () {
+  if (Menu.classList.contains("MenuVisible")) {
+    Menu.classList.add("MenuInvisible");
+    Menu.classList.remove("MenuVisible");
+    headerImg.classList.remove("MenuInvisible");
   }
 };
 
 btnMenu.addEventListener('click', openAndClose);
 menuClose.addEventListener('click', openAndClose);
+
+links.forEach((element) => {
+  element.addEventListener('click', closeMenuMobile);
+})
