dd4d3c84f51efbffb5a1cc0f62ff0f677c492568
Hectorjte <hectorjte@gmail.com>
2022-01-24 16:40:00 -0600

    Fix linter error

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-let btnMenu = document.getElementById('btn');
-let Menu = document.getElementById('MenuP');
-let headerImg = document.getElementById('headerImg');
-let menuClose = document.getElementById('menuClose');
+const btnMenu = document.getElementById('btn');
+const Menu = document.getElementById('MenuP');
+const headerImg = document.getElementById('headerImg');
+const menuClose = document.getElementById('menuClose');
 
 
 
 const openAndClose = function () {
-  if (Menu.classList.contains("MenuInvisible")){
-    Menu.classList.remove("MenuInvisible");
-    Menu.classList.add("MenuVisible");
-    headerImg.classList.add("MenuInvisible");
+  if (Menu.classList.contains('MenuInvisible')){
+    Menu.classList.remove('MenuInvisible');
+    Menu.classList.add('MenuVisible');
+    headerImg.classList.add('MenuInvisible');
   }
-  else if (Menu.classList.contains("MenuVisible")) {
-    Menu.classList.add("MenuInvisible");
-    Menu.classList.remove("MenuVisible");
-    headerImg.classList.remove("MenuInvisible");
+  else if (Menu.classList.contains('MenuVisible')) {
+    Menu.classList.add('MenuInvisible');
+    Menu.classList.remove('MenuVisible');
+    headerImg.classList.remove('MenuInvisible');
   }
 };
-
 
 btnMenu.addEventListener('click', openAndClose);
 menuClose.addEventListener('click', openAndClose);
