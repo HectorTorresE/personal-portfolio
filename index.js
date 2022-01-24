62eaf5e4eda3b24c88af2770f77172ffeb659b74
Hectorjte <hectorjte@gmail.com>
2022-01-24 16:43:15 -0600

    Fix linter error

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const Menu = document.getElementById('MenuP');
 const headerImg = document.getElementById('headerImg');
 const menuClose = document.getElementById('menuClose');
 
-
-
-const openAndClose = function () {
+openAndClose = function () {
   if (Menu.classList.contains('MenuInvisible')){
     Menu.classList.remove('MenuInvisible');
     Menu.classList.add('MenuVisible');
     headerImg.classList.add('MenuInvisible');
-  }
-  else if (Menu.classList.contains('MenuVisible')) {
+  } else if (Menu.classList.contains('MenuVisible')) {
     Menu.classList.add('MenuInvisible');
     Menu.classList.remove('MenuVisible');
     headerImg.classList.remove('MenuInvisible');
