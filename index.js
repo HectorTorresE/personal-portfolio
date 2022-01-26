54127aba586c793ce6386cab31fb145c81f7e14e
Hectorjte <hectorjte@gmail.com>
2022-01-26 11:50:05 -0600

    Add PopUp detail window

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const Menu = document.getElementById('MenuP');
 const headerImg = document.getElementById('headerImg');
 const menuClose = document.getElementById('menuClose');
 const links = document.querySelectorAll('.mobileMenu li a');
+const PortfolioM = document.getElementById('PortfolioM');
+const PortfolioMenu = document.getElementById('PortfolioMenu');
+const PBtn = document.getElementById('PBtn');
+const projectCloseM = document.getElementById('projectCloseM');
+
+const PortfolioD = document.getElementById('PortfolioD');
+const portfolioDMenu = document.getElementById('portfolioDMenu');
+const PDBtn = document.getElementById('PDBtn');
+const projectCloseMD = document.getElementById('projectCloseMD');
 
 const openAndClose = function () {
   if (Menu.classList.contains('MenuInvisible')) {
@@ -16,6 +25,42 @@ const openAndClose = function () {
   }
 };
 
+const openP = function () {
+  if (PortfolioM.classList.contains('MenuVisible')) {
+    PortfolioM.classList.remove('MenuVisible');
+    PortfolioM.classList.add('MenuInvisible');
+    PortfolioMenu.classList.remove('MenuInvisible');
+    PortfolioMenu.classList.add('MenuVisible');
+  }
+};
+
+const closeP = function () {
+  if (PortfolioMenu.classList.contains('MenuVisible')) {
+    PortfolioMenu.classList.remove('MenuVisible');
+    PortfolioMenu.classList.add('MenuInvisible');
+    PortfolioM.classList.remove('MenuInvisible');
+    PortfolioM.classList.add('MenuVisible');
+  }
+};
+
+const openPD = function () {
+  if (PortfolioD.classList.contains('MenuVisible')) {
+    PortfolioD.classList.remove('MenuVisible');
+    PortfolioD.classList.add('MenuInvisible');
+    portfolioDMenu.classList.remove('MenuInvisible');
+    portfolioDMenu.classList.add('MenuVisible');
+  }
+};
+
+const closePD = function () {
+  if (portfolioDMenu.classList.contains('MenuVisible')) {
+    portfolioDMenu.classList.remove('MenuVisible');
+    portfolioDMenu.classList.add('MenuInvisible');
+    PortfolioD.classList.remove('MenuInvisible');
+    PortfolioD.classList.add('MenuVisible');
+  }
+};
+
 const closeMenuMobile = function () {
   if (Menu.classList.contains('MenuVisible')) {
     Menu.classList.add('MenuInvisible');
@@ -25,6 +70,10 @@ const closeMenuMobile = function () {
 };
 
 btnMenu.addEventListener('click', openAndClose);
+PBtn.addEventListener('click', openP);
+projectCloseM.addEventListener('click', closeP);
+PDBtn.addEventListener('click', openPD);
+projectCloseMD.addEventListener('click', closePD);
 menuClose.addEventListener('click', openAndClose);
 
 links.forEach((element) => {
